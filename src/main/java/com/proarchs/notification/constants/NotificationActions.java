f33404f133078783abc6324bcc2d8da872138de2
PrvSunKS <48259084+PrvSunKS@users.noreply.github.com>
2020-12-28 13:51:11 +0530

    Code Recovery - Removing Notification API

--- src/main/java/com/proarchs/notification/constants/NotificationActions.java
+++
@@ -1,8 +0,0 @@
-package com.proarchs.notification.constants;
-
-public class NotificationActions {
-	
-	public static final String WELCOME = "TENANTREG_CREATION_WELCOME";
-	public static final String POSTVERIFICATION = "TENANTREG_POSTVERIFICATION";
-
-}
